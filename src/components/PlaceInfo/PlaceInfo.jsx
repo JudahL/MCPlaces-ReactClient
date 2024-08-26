70e4c68d7b7e223d3a8eb6bdf4539caa1f6c4a7e
JudahL <judahdslucas@gmail.com>
2024-08-26 22:46:49 +0100

    Updated Place Info page layout and added navigation buttons

--- src/components/PlaceInfo/PlaceInfo.jsx
+++ src/components/PlaceInfo/PlaceInfo.jsx
@@ -1,10 +1,11 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getPlace } from '../../api/getPlace';
 import { useEffect, useState } from 'react';
 import { ErrorDisplay } from '../Error/ErrorDisplay';
 
 function PlaceInfo() {
   const params = useParams();
+  const navigate = useNavigate();
   const [place, setPlace] = useState();
   const [error, setError] = useState(null);
 
@@ -22,15 +23,50 @@ function PlaceInfo() {
     fetchPlace();
   }, []);
 
+  function handleBackClick() {
+    navigate('/places');
+  }
+
+  function handleEditClick() {
+    navigate(`/places/${params.placeId}/edit`);
+  }
+
   let renderPlaceInfo = null;
 
   if (place != null) {
     renderPlaceInfo = (
-      <div>
-        <h2>{place.name}</h2>
-        <p>{place.description}</p>
-        <h3>Coordinates</h3>
-        <p>{`X:${place.coordinates.x}, Y:${place.coordinates.y}, Z:${place.coordinates.z}`}</p>
+      <div className="w-full">
+        <div className="mx-auto w-2/3">
+          <img
+            className="object-contain w-full max-h-[550px] min-h-80 rounded-xl bg-gray-900 text-center text-gray-100"
+            src={place.imageName}
+            alt="No Image"
+          />
+          <div className="flex justify-between">
+            <div>
+              <h2 className="text-2xl text-gray-700 font-bold">{place.name}</h2>
+              <p className="text-lg text-gray-600">{place.description}</p>
+            </div>
+            <div>
+              <h3 className="text-xl text-gray-700 font-bold">Coordinates</h3>
+              <p className="text-lg text-gray-600">{`X:${place.coordinates.x}, Y:${place.coordinates.y}, Z:${place.coordinates.z}`}</p>
+            </div>
+          </div>
+          <div className="mt-8 flex justify-between gap-8">
+            <button
+              className="p-2 w-full rounded-lg bg-gray-500 text-gray-50 text-xl hover:bg-gray-400"
+              onClick={handleBackClick}
+            >
+              Back
+            </button>
+            <button
+              className="p-2 w-full rounded-lg bg-emerald-700 text-gray-50 text-xl hover:bg-emerald-600"
+              onClick={handleEditClick}
+            >
+              Edit
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -43,9 +79,3 @@ function PlaceInfo() {
 }
 
 export default PlaceInfo;
-
-/**
- * return (
-    
-  );
- */

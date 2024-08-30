e6f271a6e8fb01c227a33c8e68228c21d1442362
JudahL <judahdslucas@gmail.com>
2024-08-30 04:40:05 +0100

    Improved navbar layout for small devices

--- src/components/NavBar/Navigation.jsx
+++ src/components/NavBar/Navigation.jsx
@@ -2,7 +2,7 @@ import { NavigationButton } from './NavigationButton';
 
 function Navigation() {
   return (
-    <div className="my-auto flex justify-end gap-2">
+    <div className="my-auto flex justify-center md:justify-end gap-2">
       <NavigationButton linkTo="places" title="Places" />
       <NavigationButton linkTo="servers" title="Servers" />
       <NavigationButton linkTo="create-place" title="Create" />
--- src/components/NavBar/SiteTitle.jsx
+++ src/components/NavBar/SiteTitle.jsx
@@ -2,7 +2,7 @@ import { SiteLogo } from './SiteLogo';
 
 function SiteTitle() {
   return (
-    <div className="my-auto flex gap-2">
+    <div className="my-auto flex justify-center md:justify-normal gap-2">
       <SiteLogo />
       <h2 className="my-auto font-bold text-xl text-slate-900">
         Cubic Locales

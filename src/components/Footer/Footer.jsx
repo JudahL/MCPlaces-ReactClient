cabe5252367b7264a51a2ce311e2a24e37256c3f
JudahL <judahdslucas@gmail.com>
2024-09-20 01:50:45 +0100

    Added footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="mx-auto h-20 max-w-[1440px]">
+      <hr className="w-1/2 mx-auto h-px bg-gray-300 border-0"></hr>
+      <p className="mt-4 text-center text-emerald-700">
+        Created by Judah Lucas.
+      </p>
+    </footer>
+  );
+}
+
+export { Footer };
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { NavBar } from './NavBar/NavBar';
+import { Footer } from './Footer/Footer';
 
 function Root() {
   return (
-    <div className="bg-slate-50 min-h-dvh">
-      <div className="p-2 sm:p-4 md:p-6 lg:p-8">
+    <div className="bg-slate-50 min-h-dvh w-dvw">
+      <div className="min-h-[calc(100dvh-5rem)] p-2 sm:p-4 md:p-6 lg:p-8">
         <NavBar />
         <div className="max-w-[1440px] mx-auto py-4">
           <Outlet />
         </div>
       </div>
+      <Footer />
     </div>
   );
 }

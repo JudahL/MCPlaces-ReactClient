5412e4291217f49182f503d188de399ac9e79f21
JudahL <judahdslucas@gmail.com>
2024-09-22 13:21:05 +0100

    Updated footer and also added version numbers to better track future updates

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 function Footer() {
   return (
     <footer className="mx-auto h-20 max-w-[1440px]">
-      <hr className="w-1/2 mx-auto h-px bg-gray-300 border-0"></hr>
-      <p className="mt-4 text-center text-emerald-700">
-        Created by Judah Lucas.
+      <hr className="w-1/2 mx-auto border-gray-300"></hr>
+      <p className="mt-3 text-md text-center text-emerald-700">
+        v{import.meta.env.VITE_APP_VERSION}
+      </p>
+      <p className="mt-1 text-sm text-center text-gray-500">
+        Created by{' '}
+        <strong className="font-semibold text-emerald-700">Judah Lucas</strong>.
       </p>
     </footer>
   );
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -4,7 +4,7 @@ import { Footer } from './Footer/Footer';
 
 function Root() {
   return (
-    <div className="bg-slate-50 min-h-dvh w-dvw">
+    <div className="bg-slate-50 min-h-dvh w-dvw overflow-hidden">
       <div className="min-h-[calc(100dvh-5rem)] p-2 sm:p-4 md:p-6 lg:p-8">
         <NavBar />
         <div className="max-w-[1440px] mx-auto py-4">
